aad38aec2e88a7dcb5d2d54df1fca4c50b6fa598
Afrem05 <zweibis5plus0@gmail.com>
2024-06-20 15:58:33 +0200

    Importing from GitHub & NPM

--- FundMe.sol
+++ FundMe.sol
@@ -2,7 +2,11 @@
 
 pragma solidity ^0.8.8;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
+
+		uint256 public minimumUsd = 50;
 		
 		// make a function payable
 		function fund() public payable {
@@ -15,6 +19,17 @@ contract FundMe {
 				require(msg.value > 1e18, "Didn't send enough"); // 1e18 = 1 * 10 ** 18
 		
 		}
+		
+		function getPrice() public {
+		 // use chainlink data feeds to get price information
+		 
+		 // ABI
+		 // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+		 
+		 // With the address and Abi we can do api calls
+		}
+		
+		function getConversationRate() public {}
 		
 		function withdraw() {
 		
