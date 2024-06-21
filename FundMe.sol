391d2af704e434c6e29b94b607735531049b343a
Afrem05 <zweibis5plus0@gmail.com>
2024-06-21 07:51:00 +0200

    Libraries

--- FundMe.sol
+++ FundMe.sol
@@ -2,9 +2,11 @@
 
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+
+	using PriceConverter for uint256;
     uint256 public minimumUsd = 50 * 1e18; // 1*10**18
 
     address[] public funders;
@@ -18,9 +20,10 @@ contract FundMe {
         // send at least 1 ether with require
         // if not it will revert to an error message
         require(
-            getConversionRate(msg.value) >= minimumUsd,
-            "Didn't send enough"
-        ); // 1e18 = 1 * 10 ** 18
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        // 1e18 = 1 * 10 ** 18
         // 18 decimals places
         // msg.value is a uint56 and price is int256
 
@@ -28,41 +31,6 @@ contract FundMe {
         // Keep track of all the different funders
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
-    }
-    // turns a uint256 a price of ETH in terms of USD
-    function getPrice() public view returns (uint256) {
-        // use chainlink data feeds to get price information
-
-        // ABI
-        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-
-        // With the address and Abi we can do api calls
-
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        // 8 decimals places
-        // match 8 decimals and 18
-        // typecasting
-        return uint256(price * 1e10); // 1e10 == 10000000000
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        return priceFeed.version();
-    }
-
-    function getConversationRate(
-        uint256 ethAmount
-    ) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        // eth Price: 3000_18 zeros = ETH/USD price
-        // send 1 eth
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
     }
 
     function withdraw() {}
